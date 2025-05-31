474a1ae782a22c8a35b5de65ad7b5fea6d4fb09b
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-05-31 17:08:52 +0700

    feat : Admin authorization (#7)

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import DashboardPage from "@/features/admin/dashboard";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const session = await auth();
+
+  if (!session) return redirect("/login");
+  if (session.user.role !== "ADMIN" && session.user.role !== "OUTLET_ADMIN")
+    redirect("/");
+  
   return <DashboardPage />;
 };
 
